a12052eea7e210ffe6fc75efd266109af63f9ad0
unknown <slashley127@gmail.com>
2022-07-15 21:22:23 -0500

    first commit on assignment 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { OrbitListComponent } from './orbit-list/orbit-list.component';
 import { OrbitCountsComponent } from './orbit-counts/orbit-counts.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
